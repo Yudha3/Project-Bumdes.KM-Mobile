c7cb98f6e9abd3b87374a34482109332ff469777
yogandrn <75407016+yogandrn@users.noreply.github.com>
2022-04-20 13:39:21 +0700

    Add files via upload

---
+++ app/src/main/java/com/yogandrn/bumdeskm/Model/ResponseOngkir.java
@@ -0,0 +1,22 @@
+package com.yogandrn.bumdeskm.Model;
+
+public class ResponseOngkir {
+    String estimasi;
+    int ongkir;
+
+    public String getEstimasi() {
+        return estimasi;
+    }
+
+    public void setEstimasi(String estimasi) {
+        this.estimasi = estimasi;
+    }
+
+    public int getOngkir() {
+        return ongkir;
+    }
+
+    public void setOngkir(int ongkir) {
+        this.ongkir = ongkir;
+    }
+}
