9bd354b5f285b9d1d05ed0d8f1b816a9bf9d9c4d
yogandrn <75407016+yogandrn@users.noreply.github.com>
2022-04-20 13:26:19 +0700

    Update config.php

--- AndroidAPI/config.php
+++ AndroidAPI/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "u1694897_c_reg_5";
-$password = "jtipolije";
-$db   = "u1694897_c_reg_5_db";
+$user = "*******";
+$password = "********";
+$db   = "**********";
 
 $conn = mysqli_connect($host, $user, $password, $db) or die("Gagal menghubungi server!");;
 
